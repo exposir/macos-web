beea72ae6783d61bd9bf381884cbe34e11237dbf
menglingyu <897205285@qq.com>
2022-01-06 21:25:40 +0800

    feat: tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Desktop from "./components/Desktop";
-// import "./App.css";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="container mx-auto">
       <Desktop />
     </div>
   );
