4e7a42ae2795338401ff9b768bb0c0a90546e9d7
menglingyu <897205285@qq.com>
2022-01-09 14:47:29 +0800

    update

--- src/components/Dock/Dock.tsx
+++ src/components/Dock/Dock.tsx
@@ -1,6 +1,19 @@
 import moduleCss from "./Dock.module.scss";
+import DockItem from "../DockItem/DockItem";
+import canader from "../../asset/canader.png";
 
 function Dock() {
-  return <div className={moduleCss.container}></div>;
+  return (
+    <div className={moduleCss.Dock}>
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+      <DockItem src={canader} />
+    </div>
+  );
 }
 export default Dock;
